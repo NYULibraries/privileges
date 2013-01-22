91730fd2e752dfe45f61d509e189805b52b930a4
B Alter <barnaby.alter@gmail.com>
2013-01-22 13:46:49 -0500

    updated secret token status

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PrivilegesGuide::Application.config.secret_token = Settings.secret_token || '9f35d644c9e64245a7c54bf3d738a4159b79cccf684c2fdedfdfc4dfdadbfd3bd20ed38e72e5013001fdea7d438ea92310a686fe51b835c19d6ea785056695d0'
+PrivilegesGuide::Application.config.secret_token = Settings.secret_token
