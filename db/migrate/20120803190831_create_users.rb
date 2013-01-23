e1c4b59de99b14a3c91634a39024eef01bf4ede1
B Alter <barnaby.alter@gmail.com>
2013-01-23 13:53:07 -0500

    Updated database

--- db/migrate/20120803190831_create_users.rb
+++ db/migrate/20120803190831_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def self.up
+    #drop_table :users
     create_table :users do |t|
       t.string :username
       t.string :email
@@ -21,5 +22,9 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
+  end
+  
+  def self.down
+    drop_table :users
   end
 end
