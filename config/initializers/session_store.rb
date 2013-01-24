7ff03b85ab667724b84e9ecd6d6a600d63193ce5
B Alter <barnaby.alter@gmail.com>
2013-01-24 17:39:53 -0500

    Updated session to be domain sensitive

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-#PrivilegesGuide::Application.config.session_store :cookie_store, key: '_privileges_guide_session'
+PrivilegesGuide::Application.config.session_store :cookie_store, key: '_privileges_guide_session', :domain => Settings.cookie.domain
 
 #PrivilegesGuide::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 10.seconds
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-PrivilegesGuide::Application.config.session_store :active_record_store
+# PrivilegesGuide::Application.config.session_store :active_record_store
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124223006) do
+ActiveRecord::Schema.define(:version => 20121008215927) do
 
   create_table "application_details", :force => true do |t|
     t.string   "purpose"
@@ -79,16 +79,6 @@ ActiveRecord::Schema.define(:version => 20130124223006) do
     t.boolean  "visible",    :default => true
     t.integer  "sort_order"
   end
-
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sublibraries", :force => true do |t|
     t.string   "code"
