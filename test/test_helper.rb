49547939749b39e48421891c4cbd24c0c1e6f931
B Alter <barnaby.alter@gmail.com>
2013-03-06 11:17:04 -0500

    VCR tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,10 @@ unless ENV['CI']
   require 'simplecov-rcov'
   SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
   SimpleCov.start
-  require 'coveralls'
-  Coveralls.wear!
 end
+
+require 'coveralls'
+Coveralls.wear!
 
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
@@ -24,4 +25,32 @@ class ActiveSupport::TestCase
   PatronStatus.reindex
   PatronStatusPermission.reindex
   Sublibrary.reindex
+end
+
+# VCR is used to 'record' HTTP interactions with
+# third party services used in tests, and play em
+# back. Useful for efficiency, also useful for
+# testing code against API's that not everyone
+# has access to -- the responses can be cached
+# and re-used. 
+require 'vcr'
+require 'webmock'
+
+# To allow us to do real HTTP requests in a VCR.turned_off, we
+# have to tell webmock to let us. 
+WebMock.allow_net_connect!
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/vcr_cassettes'
+  # webmock needed for HTTPClient testing
+  c.hook_into :webmock 
+  c.default_cassette_options = {
+    :match_requests_on => [:method, VCR.request_matchers.uri_without_param(:ctx_tim)]
+  }  
+  # c.debug_logger = $stderr
+  #Rc.filter_sensitive_data("aleph.library.edu") { @@aleph_url }
+  #Rc.filter_sensitive_data("BOR_ID") { @@aleph_bor_id }
+  #Rc.filter_sensitive_data("VERIFICATION") { @@aleph_verification }
+  #Rc.filter_sensitive_data("primo.library.edu") { @@primo_url }
+  #Rc.filter_sensitive_data("solr.library.edu") { @@solr_url }
 end
