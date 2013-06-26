a1957eced87997ceabba2c5220041ac6fa9aa2ef
B Alter <barnaby.alter@gmail.com>
2013-06-26 12:25:46 -0400

    Application title :space_invader

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def application_title
+    get_formatted_detail('page_title')
+  end
+
   # Print formatted page title as application name
   def application_name
     get_formatted_detail('page_title')
