f7e2bd7b609fae74e3adf02773f64ad31496bc01
B Alter <barnaby.alter@gmail.com>
2013-12-05 13:00:09 -0500

    Standardized labelling of qa and staging envs

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,2 +1,4 @@
 set :rails_env, "qa"
 set :branch, "development-alephbeta"
+set :app_title, "privileges_qa"
+set :repository, "git@github.com:NYULibraries/privileges.git" 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,2 @@
 set :rails_env, "staging"
 set :branch, "development-elasticsearch"
-set :app_title, "privilegesdev"
-set :repository, "git@github.com:NYULibraries/privileges.git" 
