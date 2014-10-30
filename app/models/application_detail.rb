a33e97a7b0de3527dc9effe5e229747f567e4777
Scot Dalton <scotdalton@gmail.com>
2014-10-30 14:09:33 -0400

    Calling ActiveRecord#default_scope without a closure has been deprecated in Rails 4

--- app/models/application_detail.rb
+++ app/models/application_detail.rb
@@ -4,5 +4,5 @@ class ApplicationDetail < ActiveRecord::Base
   validates_presence_of :description, :the_text
   validates :purpose, :uniqueness => true, :presence => true
   # Sort by description for backend display
-  default_scope :order => "description ASC"
+  default_scope { order('description ASC') }
 end
--- app/models/permission_value.rb
+++ app/models/permission_value.rb
@@ -2,7 +2,7 @@ class PermissionValue < ActiveRecord::Base
   include Utilities::Common
 
   # attr_accessible :code, :web_text, :from_aleph, :permission_code
-  default_scope :order => 'web_text ASC'
+  default_scope { order('web_text ASC') }
 
   #Validations
   validates_presence_of :code, :web_text, :permission_code
