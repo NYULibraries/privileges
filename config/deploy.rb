19c22af53d6970c67eeb3b4c75f8d48f553e9464
B Alter <barnaby.alter@gmail.com>
2014-11-11 15:49:26 -0500

    Added delayed_job startup to capistrano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,12 @@
 require 'formaggio/capistrano'
 set :app_title, "privileges"
 set :rvm_ruby_string, "ruby-2.1.3"
+
+namespace :delayed_jobs do
+  desc "Startup delayed jobs script"
+  task :restart do
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bin/delayed_job restart"
+  end
+end
+
+after "deploy", "delayed_jobs:restart"
