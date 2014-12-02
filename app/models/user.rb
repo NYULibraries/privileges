02deb7300afd0f53c01adc612d50ccb1fa7a3cc5
B Alter <barnaby.alter@gmail.com>
2014-12-02 13:41:21 -0500

    Added devise config to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-
+  # Login with NYULibraris OAuth provider
+  devise :omniauthable, omniauth_providers: [:nyulibraries]
+  
   serialize :user_attributes
 
   acts_as_indexed fields: [:firstname, :lastname, :username, :email]
