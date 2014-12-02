21694c9aa2c2244f9716877cb03c282ac95cb612
B Alter <barnaby.alter@gmail.com>
2014-12-02 15:43:38 -0500

    Upgrade to respond_with/to syntax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,28 @@
 class UsersController < ApplicationController
   before_filter :authenticate_admin
+  respond_to :html, :json
+  respond_to :csv, only: [:index]
 
   # GET /users
   def index
     @users = User.order(sort_column + " " + sort_direction).page(params[:page]).per(30)
     @users = @users.with_query(params[:q]) unless params[:q].blank?
 
-    respond_to do |format|
-      format.html
+    respond_with(@users) do |format|
       format.csv { render :csv => @users, :filename => "privileges_guide_users" }
     end
   end
 
   # GET /users/1
   def show
-    @user = User.find_by_username(params[:id])
-    @user = User.find(params[:id]) if @user.nil?
+    @user = (User.find_by_username(params[:id]) || User.find(params[:id]))
+    respond_with(@user)
   end
 
   # GET /users/1/edit
   def edit
     @user = User.find_by_username(params[:id])
-
-    redirect_to root_url and return
+    respond_with(@user, location: root_url)
   end
 
   # DELETE /users/1
@@ -30,29 +30,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(users_url) }
-    end
+    respond_with(@user)
   end
 
   # PUT /users/1
   def update
     @user = User.find(params[:id])
-    user_attributes = {}
-    user_attributes[:access_grid_admin] = params[:user][:access_grid_admin].to_i == 1
-    @user.update_attributes(:user_attributes => user_attributes)
+    @user.admin = (user_params[:admin].to_i == 1)
+    @user.save
 
-    respond_to do |format|
-      format.js { render :layout => false } if request.xhr?
-      format.html { redirect_to(@user) }
-    end
+    respond_with(@user)
   end
 
   # Delete all non-admin patrons
   def clear_patron_data
-    User.destroy_all("user_attributes not like '%:access_grid_admin: true%'")
-    flash[:success] = t('users.clear_patron_data_success')
-    redirect_to users_url and return
+    @users = User.non_admin
+    flash[:success] = t('users.clear_patron_data_success') if @users.destroy_all
+    respond_with(@users, location: users_url)
   end
 
   # Implement sort column function for this model
@@ -60,5 +54,10 @@ class UsersController < ApplicationController
     super "User", "lastname"
   end
   helper_method :sort_column
+
+  def user_params
+    params.require(:user).permit(:admin)
+  end
+  private :user_params
 
 end
