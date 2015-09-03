7b651ecb6ed5fa5b18882934f870c86fed294596
B Alter <barnaby.alter@gmail.com>
2015-09-03 12:06:23 -0400

    Load env vars before bundle

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,13 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
 require 'figs'
 # Don't run this initializer on travis.
 Figs.load(stage: Rails.env) unless ENV['TRAVIS']
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module PrivilegesGuide
   LOCAL_CREATION_PREFIX = 'nyu_ag_noaleph_'
