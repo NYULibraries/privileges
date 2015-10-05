8d7bedcadcdf3aef1f1e066c4afac60badf80049
B Alter <barnaby.alter@gmail.com>
2015-10-05 08:52:56 -0400

    Only populate from user attributes if it exists

--- db/migrate/20141202185852_populate_user_fields_from_user_attributes.rb
+++ db/migrate/20141202185852_populate_user_fields_from_user_attributes.rb
@@ -3,10 +3,12 @@ class PopulateUserFieldsFromUserAttributes < ActiveRecord::Migration
     say_with_time "Migrating User Attributes" do
       User.class_eval { serialize :user_attributes }
       User.all.each do |user|
-        user.update_attribute :aleph_id, user.user_attributes[:nyuidn]
-        user.update_attribute :patron_status, user.user_attributes[:bor_status]
-        user.update_attribute :admin, (user.user_attributes[:access_grid_admin].nil?) ? false : user.user_attributes[:access_grid_admin]
-        user.update_attribute :institution_code, user.user_attributes[:primary_institution]
+        unless user.user_attributes.blank?
+          user.update_attribute :aleph_id, user.user_attributes[:nyuidn]
+          user.update_attribute :patron_status, user.user_attributes[:bor_status]
+          user.update_attribute :admin, (user.user_attributes[:access_grid_admin].nil?) ? false : user.user_attributes[:access_grid_admin]
+          user.update_attribute :institution_code, user.user_attributes[:primary_institution]
+        end
       end
     end
     say_with_time "Removing User Attributes" do
