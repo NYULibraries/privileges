3c5a4eaf833000645fa117e3c34f67273760ea81
Barnaby Alter <barnaby.alter@nyu.edu>
2017-09-07 13:10:29 -0400

    Manually include all the formaggio recipes we're using, notably excluding new relic

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,16 @@
-# Default require
-require 'formaggio/capistrano'
+# Manually include all recipes we're using
+require 'formaggio/capistrano/multistage'
+require 'formaggio/capistrano/default_attributes'
+require 'formaggio/capistrano/figs'
+require 'formaggio/capistrano/config'
+require 'formaggio/capistrano/assets'
+require 'formaggio/capistrano/bundler'
+require 'formaggio/capistrano/rvm'
+require 'formaggio/capistrano/server/passenger'
+require 'formaggio/capistrano/cache'
+require 'formaggio/capistrano/cleanup'
+require 'formaggio/capistrano/environment'
+
 set :app_title, "privileges"
 set :rvm_ruby_string, "ruby-2.3.4"
 set :assets_gem, ["nyulibraries_stylesheets.git", "nyulibraries_javascripts.git"]
