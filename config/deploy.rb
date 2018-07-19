fc6bb745ceba582e1a860c4b274067e86671a627
BAlter <barnaby.alter@nyu.edu>
2018-07-19 15:20:24 -0400

    Updated ruby to 2.3.6

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,5 +12,5 @@ require 'formaggio/capistrano/cleanup'
 require 'formaggio/capistrano/environment'
 
 set :app_title, "privileges"
-set :rvm_ruby_string, "ruby-2.3.4"
+set :rvm_ruby_string, "ruby-2.3.6"
 set :assets_gem, ["nyulibraries_stylesheets.git", "nyulibraries_javascripts.git"]
