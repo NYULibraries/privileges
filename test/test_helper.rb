f907b6037f02ed2942b2297e1532d2cf9126b783
Elliott Grieco <elliott.grieco@nyu.edu>
2019-03-25 14:48:58 -0400

    Implements Rails patch for other tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 class ActionController::TestCase
   include Devise::Test::ControllerHelpers
 end
