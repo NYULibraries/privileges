6dda57a7063820d5d453e317f05934fce0711a58
Elliott Grieco <elliott.grieco@nyu.edu>
2019-03-28 13:42:37 -0400

    Fixes hash rocket syntax in non-test repositories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,13 +8,13 @@ module ApplicationHelper
   # Create a select tag with permission values and select the correct one based on this PatronStatusPermission
   def get_permission_value(ps_perm)
    permission_values = ps_perm.permission.permission_values
-   select_tag "update_permission_ids[#{ps_perm.id}]", options_for_select(permission_values.collect{ |pv| [pv.web_text.html_safe, pv.id] }, {:selected => ps_perm.permission_value_id}),
+   select_tag "update_permission_ids[#{ps_perm.id}]", options_for_select(permission_values.collect{ |pv| [pv.web_text.html_safe, pv.id] }, {selected: ps_perm.permission_value_id}),
     {prompt: "Select a permission value", class: 'form-control'}
   end
 
   # Format and santitize detail from database
   def get_formatted_detail(purpose, css = nil)
-   simple_format(get_sanitized_detail(purpose), :class => css)
+   simple_format(get_sanitized_detail(purpose), class: css)
   end
 
   # Fetch application detail text by purpose
@@ -36,7 +36,7 @@ module ApplicationHelper
 
   # Sanitize HTML
   def print_sanitized_html(html)
-   sanitize(html, :tags => %w(b strong i em br p a ul li), :attributes => %w(target href class)).html_safe
+   sanitize(html, tags: %w(b strong i em br p a ul li), attributes: %w(target href class)).html_safe
   end
 
   # Format html
@@ -52,7 +52,7 @@ module ApplicationHelper
   # Generate an abbr tag for long words
   def word_break word
     if word.length > 10
-      content_tag :abbr, truncate(word, :length => 10), :title => word
+      content_tag :abbr, truncate(word, length: 10), title: word
     else
       word
     end
@@ -65,7 +65,7 @@ module ApplicationHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     direction_icon = (direction.eql? "desc") ? :sort_desc : :sort_asc
     search = params[:search]
-    html = link_to title, params.merge(:sort => column, :direction => direction, :page => nil, :id => ""), {:class => css_class}
+    html = link_to title, params.merge(sort: column, direction: direction, page: nil, id: ""), {class: css_class}
     html << icon_tag(direction_icon) if column == sort_column
     return html
   end
