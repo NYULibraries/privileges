cd6aaeb0f4023bfefee515778de30c202e13d194
Elliott Grieco <elliott.grieco@nyu.edu>
2019-03-29 16:46:44 -0400

    Fixes specs for privileges_controller

--- spec/controllers/privileges_controller_spec.rb
+++ spec/controllers/privileges_controller_spec.rb
@@ -20,7 +20,7 @@ describe PrivilegesController do
 
     context "when user signed in" do
       let(:user){ create(:user, patron_status: '23') }
-      let!(:patron_status){ create(:patron_status, code: user.patron_status) }
+      let(:patron_status){ create(:patron_status, code: user.patron_status) }
       let(:redirect_path){ patron_path("#{patron_status.id}-#{@controller.send(:urlize, patron_status.web_text)}") }
       before do
         sign_in user
@@ -28,33 +28,34 @@ describe PrivilegesController do
       end
       subject { response }
 
-      xit { is_expected.to redirect_to redirect_path }
-      xit "should assign patron_status" do
+      it { is_expected.to redirect_to redirect_path }
+      it "should assign patron_status" do
         expect(user.patron_status).to eq patron_status.code
         expect(assigns(:patron_status).stored(:code)).to eq user.patron_status
       end
     end
   end
 
-  xdescribe '#show_patron_status' do
+  describe '#show_patron_status' do
     let(:patron_status) { create(:patron_status) }
-    let(:sublibrary_code) { nil }
     before { get :show_patron_status, params: { id: patron_status.id, sublibrary_code: sublibrary_code } }
+
     subject { response }
     context 'when there is no sublibrary code' do
+      let(:sublibrary_code) { nil }
       it 'should set up variables' do
         expect(assigns(:patron_status)).to eql patron_status
-        # expect(assigns(:sublibraries_with_access)).to_not be_nil
         expect(assigns(:sublibraries)).to_not be_nil
         expect(assigns(:sublibrary)).to be_nil
         expect(subject).to render_template(:show_patron_status)
       end
     end
     context 'when there is a sublibrary code provided' do
-      let(:sublibrary_code) { create(:sublibrary).code }
+      let(:sublibrary) { create(:sublibrary) }
+      let(:sublibrary_code) { sublibrary.code }
       it 'should set up variables' do
         expect(assigns(:patron_status_permissions)).to_not be_nil
-        expect(assigns(:sublibrary)).to be_nil
+        expect(assigns(:sublibrary)).to eql sublibrary
         expect(subject).to render_template(:show_patron_status)
       end
     end
