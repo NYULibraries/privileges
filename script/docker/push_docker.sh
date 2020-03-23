da7645ce8c39a1865d82c16e20b9f8a0a99b8843
Eric Griffis <eric.griffis@nyu.edu>
2020-03-23 12:51:50 -0400

    DRY up docker-compose; use test repo for test image

--- script/docker/push_docker.sh
+++ script/docker/push_docker.sh
@@ -8,10 +8,10 @@ docker push quay.io/nyulibraries/privileges:latest
 docker push quay.io/nyulibraries/privileges:${CIRCLE_BRANCH//\//_}
 docker push quay.io/nyulibraries/privileges:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
-docker tag privileges_dev quay.io/nyulibraries/privileges:dev-latest
-docker tag privileges_dev quay.io/nyulibraries/privileges:dev-${CIRCLE_BRANCH//\//_}
-docker tag privileges_dev quay.io/nyulibraries/privileges:dev-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+docker tag privileges_dev quay.io/nyulibraries/privileges_test:latest
+docker tag privileges_dev quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}
+docker tag privileges_dev quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
-docker push quay.io/nyulibraries/privileges:dev-latest
-docker push quay.io/nyulibraries/privileges:dev-${CIRCLE_BRANCH//\//_}
-docker push quay.io/nyulibraries/privileges:dev-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+docker push quay.io/nyulibraries/privileges_test:latest
+docker push quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}
+docker push quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
