b1e3fa6b14ed7f7c2682c459e90b2099de9c7ca4
Eric Griffis <eric.griffis@nyu.edu>
2021-04-29 16:07:15 -0400

    Create deploy service, unify cap branch configuration under env var; use new jenkins for deploys in circle

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,4 +1,3 @@
 set :rails_env, "qa"
-set(:branch, ENV["GIT_BRANCH"].gsub(/remotes\//,"").gsub(/origin\//,""))
 set :app_title, "privileges_qa"
 set :repository, "git@github.com:NYULibraries/privileges.git"
