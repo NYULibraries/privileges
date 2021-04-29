fa04f614d1979a685e429144e078bb262b4616e0
Eric Griffis <eric.griffis@nyu.edu>
2021-04-29 15:45:07 -0400

    Fix docker push script

--- script/docker/push_docker.sh
+++ script/docker/push_docker.sh
@@ -8,9 +8,9 @@ docker push quay.io/nyulibraries/privileges:latest
 docker push quay.io/nyulibraries/privileges:${CIRCLE_BRANCH//\//_}
 docker push quay.io/nyulibraries/privileges:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
-docker tag privileges_dev quay.io/nyulibraries/privileges_test:latest
-docker tag privileges_dev quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}
-docker tag privileges_dev quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+docker tag privileges_test quay.io/nyulibraries/privileges_test:latest
+docker tag privileges_test quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}
+docker tag privileges_test quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
 docker push quay.io/nyulibraries/privileges_test:latest
 docker push quay.io/nyulibraries/privileges_test:${CIRCLE_BRANCH//\//_}
